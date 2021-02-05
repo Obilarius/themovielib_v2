98e3d651d80d614c8fd3d9e0a987bca8ff2f7423
Obilarius <git@walzenbach.com>
2021-02-05 18:50:14 +0100

    Initial

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (  
+    <nav className="navbar">
+      <div className="container">
+        <h1 className="brand">TheMobieLib</h1>
+
+        <div className="links">
+          <div className="link">Diary</div>
+          <div className="link">Profil</div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;

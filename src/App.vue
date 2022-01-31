dbd826fb35f3ddce4eeaab8547b6e32bb8abace1
Obilarius <git@walzenbach.com>
2022-01-31 17:52:29 +0100

    Finish 🎉🥳

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <MyForm msg="Hello Vue 3 + Vite" />
+  <nav>
+    <router-link to="/">Home</router-link> | <router-link to="/feed">Feed</router-link> |
+    <router-link to="/register">Register</router-link> | <router-link to="/sign-in">Login</router-link> |
+    <button @click="handleSignOut" v-if="isLoggedIn">Sign out</button>
+  </nav>
+  <router-view />
 </template>
 
 <script setup>
-import MyForm from "./components/MyForm.vue";
+import { onMounted, ref } from "vue"
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth"
+import { useRouter } from "vue-router"
+
+const router = useRouter() // get a reference to our vue router
+const isLoggedIn = ref(false)
+
+let auth
+onMounted(() => {
+  auth = getAuth()
+  onAuthStateChanged(auth, (user) => {
+    if (user) isLoggedIn.value = true
+    else isLoggedIn.value = false
+  })
+})
+
+const handleSignOut = () => {
+  signOut(auth).then(() => {
+    router.push("/")
+  })
+}
 </script>
 
 <style>
